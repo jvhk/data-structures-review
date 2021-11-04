a2d8faba765ba8f670bea479414df2ab0651792c
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-11-03 22:49:12 -0300

    add: constants

--- abstract_types/main.cpp
+++ abstract_types/main.cpp
@@ -1,45 +1,19 @@
 #include <cstdlib>
 #include <iostream>
 
+#define INICIO 1
+#define FIM 1000
+
 using namespace std;
 
-typedef enum {dom, seg, ter, qua, qui, sex, sab} dias_semana;
+
 
 int main(int argc, char const *argv[])
-{
-    dias_semana dia;
-    int num;
+{    
+    int i;
 
-    cout << "Digite um numero de 1 a 7: ";
-    cin>> num;
-
-    switch (num -1)
-    {
-    case dom:
-        cout << "Domingo";
-        break;
-    case seg:
-        cout << "Segunda-feira";
-        break;
-    case ter:
-        cout << "Terça-feira";
-        break;
-    case qua:
-        cout << "Quarta-feira";
-        break;
-    case qui:
-        cout << "Quinta-feira ";
-        break;
-    case sex:
-        cout << "Sexta-feira";
-        break;
-    case sab:
-        cout << "Sábado ";
-        break;
-    default:
-        break;
-    }
-
-    cout<<endl;
+    for(i= INICIO; i <= FIM; i++)
+        cout<< "\n" << i;
+        
     return 0;
 }
